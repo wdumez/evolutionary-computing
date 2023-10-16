f370b93b8a01a7cfca8162bdf9f005dd141a295e
William Dumez <william.dumez@skynet.be>
2023-10-16 12:22:40 +0200

    Implemented PMX

--- r0758170.py
+++ r0758170.py
@@ -5,21 +5,50 @@ import Reporter
 
 def mutate_inversion(candidate):
     """Mutate a candidate solution in place using inversion mutation."""
-    if candidate.size <= 1:
+    size = candidate.size
+    if size <= 1:
         return
-    first_pos = rd.randint(0, candidate.size - 2)
-    second_pos = rd.randint(first_pos, candidate.size - 1)
+    first_pos = rd.randint(0, size - 2)
+    second_pos = rd.randint(first_pos, size - 1)
     candidate[first_pos:second_pos + 1] = np.flip(candidate[first_pos:second_pos + 1])
 
 
 def recombine_PMX(parent1, parent2):
-    """Use two parent candidates to produce offspring using partially mapped crossover."""
-    raise NotImplementedError
+    """Use two parent candidates to produce offspring using partially mapped crossover.
+    See p.70-71 in Eiben & Smith.
+    """
+    size = parent1.size
+    offspring = np.zeros_like(parent1)
+    # We must initialize offspring with -1's, to identify whether a spot is not yet filled.
+    for i in range(size):
+        offspring[i] = -1
+    # 1.
+    first_pos = rd.randint(0, size-2)
+    second_pos = rd.randint(first_pos, size-1)
+    offspring[first_pos:second_pos+1] = parent1[first_pos:second_pos+1]
+    # 2. -  5.
+    for elem in parent2[first_pos:second_pos+1]:
+        if elem in parent1[first_pos:second_pos+1]:
+            continue  # elem already occurs in offspring
+        # elem is not yet in offspring, find the index to place it
+        index = 0
+        value = elem
+        while value != -1:
+            index = index_of(parent2, value)
+            value = offspring[index]
+        offspring[index] = elem
+    # 6.
+    for i in range(size):
+        if offspring[i] == -1:
+            offspring[i] = parent2[i]
+    return offspring
 
 
 def index_of(array, value):
     """Return the first index at which value occurs in array."""
-    return np.where(array, value)[0][0]
+    tmp = np.where(array == value)
+    return tmp[0][0]
+    # return np.where(array == value)[0][0]
 
 
 # Modify the class name to match your student number.
