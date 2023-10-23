7d24905c34241acdfaaca79c8bb1413306a1e902
William Dumez <william.dumez@skynet.be>
2023-10-23 22:35:37 +0200

    Implemented edge crossover

--- r0758170.py
+++ r0758170.py
@@ -14,6 +14,79 @@ def mutate_inversion(candidate):
     candidate[first_pos:second_pos + 1] = np.flip(candidate[first_pos:second_pos + 1])
 
 
+def recombine_edge_crossover(parent1, parent2):
+    """Use two parent candidates to produce offspring using edge crossover."""
+    # 1. Construct the adjacency table.
+    adj_table = create_adj_table(parent1, parent2)
+    choices = [x for x in parent1]
+    partial_result = []
+    # 2 & 3. Pick an initial element at random and put it in the offspring.
+    #        Set the variable current_element = entry.
+    current_element = rd.choice(choices)
+    while len(choices) != 0:
+        choices.remove(current_element)
+        partial_result.append(current_element)
+        # 4. Remove all references to current_element from the table.
+        for x, adj_list in adj_table.items():
+            for y, is_common in adj_list:
+                if current_element == y:
+                    adj_list.remove((y, is_common))
+        # 5. Examine list for current element.
+        current_element_adj_list = adj_table[current_element]
+        # If there is a common edge, pick that to be the next element.
+        next_element = None
+        for x, is_common in current_element_adj_list:
+            if is_common:
+                next_element = x
+                break
+        if not next_element:
+            # Otherwise pick the entry in the list which itself has the shortest list.
+            options = []
+            shortest_length = math.inf
+            for x, adj_list in adj_table.items():
+                if x not in [y for y, _ in current_element_adj_list]:
+                    continue
+                if len(adj_list) <= shortest_length:
+                    shortest_length = len(adj_list)
+                    options.append(x)
+            # 6. In the case of an empty list, the other end of the offspring is examined for extension;
+            # otherwise a new element is chosen at random.
+            if len(options) == 0:
+                # TODO I don't know what that means? Just picking at random for now.
+                if len(choices) == 0:
+                    break
+                next_element = rd.choice(choices)
+            else:
+                next_element = rd.choice(options)
+        current_element = next_element
+    return np.array(partial_result)
+
+
+def create_adj_table(candidate1, candidate2):
+    """Create an adjacency table for candidate1 and candidate2."""
+    adj_table = {x: [] for x in candidate1}
+    for x in adj_table:
+        adj_in_parent1 = get_adj(x, candidate1)
+        adj_in_parent2 = get_adj(x, candidate2)
+        for y in adj_in_parent1:
+            if y in adj_in_parent2:
+                adj_table[x].append((y, True))
+                adj_in_parent2.remove(y)
+            else:
+                adj_table[x].append((y, False))
+        for y in adj_in_parent2:
+            adj_table[x].append((y, False))
+    return adj_table
+
+
+def get_adj(x, candidate):
+    """Returns the adjacent values of x in candidate as a list."""
+    x_idx = index_of(candidate, x)
+    prev_idx = x_idx-1
+    next_idx = x_idx+1 if x_idx < candidate.size-1 else 0
+    return [candidate[prev_idx], candidate[next_idx]]
+
+
 def recombine_PMX(parent1, parent2):
     """Use two parent candidates to produce one offspring using partially mapped crossover.
     See p.70-71 in Eiben & Smith.
